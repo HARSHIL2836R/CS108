0fafd373252355df660051b8e5583d08a01943e3
HARSHIL2836R <harshilsolanki1912@gmail.com>
2024-03-26 11:26:35 +0530

    class notes

--- CS104_Spring_2022_Resources-main/CS104_Spring_2022_Resources-main/Lab Exams/Final/Final Exam/GDB/main.cpp
+++ CS104_Spring_2022_Resources-main/CS104_Spring_2022_Resources-main/Lab Exams/Final/Final Exam/GDB/main.cpp
@@ -17,13 +17,13 @@ int main()
 
     for(i=0; i<n-1; i++)
     {
-   	 for(j=0; j<i; j++)
-   	 {
-   		 if(arr[j]<arr[j+1])
+   	 for(j=0; j<i+1; j++)
+   	 {  
+       if(arr[j]>arr[j+1])
    		 {
        		 temp = arr[j];
-   			 arr[j+1] = temp;
-       		 arr[j] = arr[j+1];
+   			 arr[j] = arr[j+1];
+       		 arr[j+1] = temp;
    		 }
    	 }
     }
